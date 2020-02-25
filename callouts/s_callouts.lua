61efffd6911b82e035325dd45bd6e77c1bdeccc5
GalaxHD551 <59099660+GalaxHD551@users.noreply.github.com>
2020-02-26 02:10:10 +0400

    Update s_callouts.lua

--- callouts/s_callouts.lua
+++ callouts/s_callouts.lua
@@ -1,166 +1,331 @@
 local _ = function(k, ...) return ImportPackage("i18n").t(GetPackageName(), k, ...) end
 
-local callOuts = {}
+local MAX_TAXI = 10 -- Number of taximens at the same time
+local ALLOW_RESPAWN_VEHICLE = true -- Allow the respawn of the vehicle by destroying the previously spawned one. (Can break RP if the car is stolen or need repairs or fuel)
+local CAUTION = 1500 -- Amount of the commission
+local state = false
+local TaxiState = false
 
-AddCommand("med", function(player, label)
-    CreateCallout(player, "medic", tostring(label))
-end)
+local occupants = {}
 
-AddCommand("911", function(player, label)
-    CreateCallout(player, "police", tostring(label))
-end)
+local VEHICLE_SPAWN_LOCATION = {
+    {x = 177218, y = 159869, z = 4817, h = -70},
+}
 
+local TAXI_SERVICE_NPC = {
+    {x = 176053, y = 158567, z = 4850, h = 0},
+}
 
---------- CALLOUTS
-AddCommand("clearcallouts", function(player)
-    if PlayerData[player].admin ~= 1 then return end
-    callOuts = {}
-    UpdateCalloutsList(player)
-    print('CALLOUTS → Cleared')
-end)
+local TAXI_VEHICLE_NPC = {
+    {x = 176211, y = 159760, z = 4818, h = 10},
+}
 
-function CreateCallout(player, job, label)-- create a new callout
-    label = string.gsub(label, '"', '')
-    if callOuts[player] ~= nil and callOuts[player].taken ~= false then return end
-    local x, y, z = GetPlayerLocation(player)
-    local caller_job = nil
+local TAXI_GARAGE = {
+    {x = 175676, y = 159208, z = 4819},
+}
 
-    if PlayerData[player].job == "police" then
-        caller_job = 'police'
-    elseif PlayerData[player].job == "medic" then
-        caller_job = 'medic'
+local taxiNpcIds = {}
+local taxiVehicleNpcIds = {}
+local taxiGarageIds = {}
+
+AddEvent("OnPackageStart", function()
+    for k, v in pairs(TAXI_SERVICE_NPC) do
+        v.npcObject = CreateNPC(v.x, v.y, v.z, v.h)
+        CreateText3D(_("taxi_job") .. "\n" .. _("press_e"), 18, v.x, v.y, v.z + 120, 0, 0, 0)
+        SetNPCAnimation(v.npcObject, "CROSSARMS", true)     
+        table.insert(taxiNpcIds, v.npcObject)
     end
-
-    callOuts[player] = { location = {x = x, y = y, z = z}, taken = false, job = job, label = label, caller_job = caller_job }
-    CalloutsNotifyPlayers(callOuts[player])
-    UpdateCalloutsList(player, job)
-
-    if job == "medic" then
-        CallRemoteEvent(player, "MakeNotification", _("medic_callout_created"), "linear-gradient(to right, #00b09b, #96c93d)", 10000)
-    elseif job == "police" then
-        CallRemoteEvent(player, "MakeNotification", _("police_callout_created"), "linear-gradient(to right, #00b09b, #96c93d)", 10000)
-    else
-        CallRemoteEvent(player, "MakeNotification", _("callout_created"), "linear-gradient(to right, #00b09b, #96c93d)", 10000)
-    end
-end
-AddRemoteEvent("callouts:create", CreateCallout)
-
-function CalloutsNotifyPlayers(callout)-- send the new callout to medics and policemens
-    for k, v in pairs(GetAllPlayers()) do
-        if PlayerData[v] ~= nil and PlayerData[v].job ~= nil and PlayerData[v].job ~= "" and callout.job == PlayerData[v].job then
-            if callout.job == "medic" then
-                CallRemoteEvent(v, "MakeNotification", _("medic_new_callout", callout.label), "linear-gradient(to right, #00b09b, #96c93d)", 10000)
-                CallRemoteEvent(v, "medic:deathalarm")         
-            elseif callout.job == "police" then
-                CallRemoteEvent(v, "MakeNotification", _("police_new_callout", callout.label), "linear-gradient(to right, #00b09b, #96c93d)", 10000)
-                CallRemoteEvent(v, "medic:deathalarm")
-            else  
-                CallRemoteEvent(v, "MakeNotification", _("new_callout", callout.label), "linear-gradient(to right, #00b09b, #96c93d)", 10000)
-            end
-        end        
-    end
-end
-
-function CalloutTake(player, target)-- allow  to take the callout
-    if PlayerData[player].job ~= "medic" and PlayerData[player].job ~= "police" then return end
-    if callOuts[tonumber(target)] == nil then return end
-
-    for k,v in pairs(callOuts) do
-        if v.taken == player then
-            CallRemoteEvent(player, "MakeErrorNotification", _("callout_already_have_callout"))
-            return
+    
+    if ALLOW_RESPAWN_VEHICLE then
+        for k, v in pairs(TAXI_GARAGE) do
+            v.garageObject = CreatePickup(2, v.x, v.y, v.z)
+            table.insert(taxiGarageIds, v.garageObject)
         end
     end
+    
+    for k, v in pairs(TAXI_VEHICLE_NPC) do
+        v.npcObject = CreateNPC(v.x, v.y, v.z, v.h)
+        CreateText3D(_("taxi_garage_menu") .. "\n" .. _("press_e"), 18, v.x, v.y, v.z + 120, 0, 0, 0)
+        SetNPCAnimation(v.npcObject, "WALLLEAN04", true)
+        table.insert(taxiVehicleNpcIds, v.npcObject)
+    end
+end)
 
-    if callOuts[tonumber(target)].taken ~= false then
-        CallRemoteEvent(player, "MakeErrorNotification", _("callout_taken_by", PlayerData[callOuts[tonumber(target)].taken].name))
+AddEvent("OnPlayerJoin", function(player)
+    CallRemoteEvent(player, "taxi:setup", taxiNpcIds, taxiGarageIds, taxiVehicleNpcIds)
+end)
+
+--------- SERVICE
+function TaxiStartStopService(player)
+    if PlayerData[player].job == "" then
+        TaxiStartService(player)
+    elseif PlayerData[player].job == "taxi" then
+        TaxiEndService(player)
+    else
+        CallRemoteEvent(player, "MakeErrorNotification", _("please_leave_previous_job"))
+    end
+end
+AddRemoteEvent("taxi:startstopservice", TaxiStartStopService)
+
+function TaxiStartService(player)-- To start the taxi service
+    -- #1 Check if the player has a job vehicle spawned then destroy it
+    if PlayerData[player].job_vehicle ~= nil then
+        DestroyVehicle(PlayerData[player].job_vehicle)
+        DestroyVehicleData(PlayerData[player].job_vehicle)
+        PlayerData[player].job_vehicle = nil
+    end
+    
+    -- #2 Check for the number of taximen in service
+    local taximens = 0
+    for k, v in pairs(PlayerData) do
+        if v.job == "taxi" then taximens = taximens + 1 end
+    end
+    if taximens >= MAX_TAXI then
+        CallRemoteEvent(player, "MakeErrorNotification", _("job_full"))
+        return
+    end
+    
+    -- #3 Check for the taxi licence
+    if PlayerData[player].taxi_license == 0 then
+        CallRemoteEvent(player, "MakeErrorNotification", _("no_taxi_licence"))
         return
     end
 
-    callOuts[tonumber(target)].taken = player
-
-    local label
-
-    if PlayerData[player].job == "medic" then
-        label = _("medic_waypoint_label")
-    elseif PlayerData[player].job == "police" then
-        label = _("police_waypoint_label")
-    else
-        label = _("callout_waypoint_label")
-    end
-
-    UpdateCalloutsList(player)
-
-    CallRemoteEvent(player, "callouts:createwp", tonumber(target), callOuts[tonumber(target)].location.x, callOuts[tonumber(target)].location.y, callOuts[tonumber(target)].location.z, label)
-    CallRemoteEvent(player, "MakeNotification", _("callouts_you_took_callout"), "linear-gradient(to right, #00b09b, #96c93d)")
-    CallRemoteEvent(tonumber(target), "MakeNotification", _("callout_has_been_taken"), "linear-gradient(to right, #00b09b, #96c93d)", 10000)
-end
-AddRemoteEvent("callouts:start", CalloutTake)
-
-function CalloutEnd(player, target)-- allow  to end a callout
-    if PlayerData[player].job ~= "medic" and PlayerData[player].job ~= "police" then return end
-    if callOuts[tonumber(target)] == nil then return end
-    if callOuts[tonumber(target)].taken ~= player then return end
-
-    callOuts[tonumber(target)] = nil
-    UpdateCalloutsList(player)
-    CallRemoteEvent(player, "callouts:cleanwp", tonumber(target))
-    CallRemoteEvent(player, "MakeNotification", _("callouts_ended_callout"), "linear-gradient(to right, #00b09b, #96c93d)")
-end
-AddRemoteEvent("callouts:end", CalloutEnd)
-
-AddRemoteEvent("callouts:getlist", function(player)
-    local calloutsList = GetCalloutsList(player)
-
-    CallRemoteEvent(player, "callouts:displaymenu", calloutsList)    
-end)
-
-function UpdateCalloutsList(player, job)
-    local job = job or PlayerData[player].job
+    -- #3 Set the player job to taxi
+    PlayerData[player].job = "taxi"
+    CallRemoteEvent(player, "taxi:client:isonduty", true)
+    CallRemoteEvent(player, "MakeNotification", _("join_taxi"), "linear-gradient(to right, #00b09b, #96c93d)")
     
-    for k, v in pairs(GetAllPlayers()) do
-        if PlayerData[v] ~= nil and PlayerData[v].job ~= nil and PlayerData[v].job ~= "" and job == PlayerData[v].job then
-            local calloutsList = GetCalloutsList(v)
-            CallRemoteEvent(v, "callouts:updatelist", calloutsList)  
+    return true
+end
+
+function TaxiEndService(player)-- To end the taxi service
+    --Set player job
+    PlayerData[player].job = ""
+    CallRemoteEvent(player, "taxi:client:isonduty", false)  
+    CallRemoteEvent(player, "MakeNotification", _("quit_taxi"), "linear-gradient(to right, #00b09b, #96c93d)")
+    
+    return true
+end
+
+--------- SERVICE END
+--------- TAXI VEHICLE
+--[[function CheckCash(player)
+        -- #1 Check if the player has the taxi job
+        if PlayerData[player].job ~= "taxi" then
+            CallRemoteEvent(player, "MakeErrorNotification", _("not_taxi"))
+            return
         end
+        
+        -- #2 Check if the player has money for commission
+        if CAUTION > GetPlayerCash(player) then
+            CallRemoteEvent(player, "MakeErrorNotification",_("no_money_car"))
+        else
+            paiement = "cash" 
+            CallEvent("SpawnTaxiCar", player, paiement)                                     CAUTION FOR TAXI VEHICLE
+        end
+end
+AddRemoteEvent("taxi:checkcash", CheckCash)       
+    
+function CheckBank(player)
+    -- #1 Check if the player has the taxi job
+    if PlayerData[player].job ~= "taxi" then
+        CallRemoteEvent(player, "MakeErrorNotification", _("not_taxi"))
+        return
+    end
+    
+    -- #2 Check if the player has money for commission
+    if CAUTION > PlayerData[player].bank_balance then
+        CallRemoteEvent(player, "MakeErrorNotification", _("no_money_car"))
+    else
+        paiement = "bank"
+        CallEvent("SpawnTaxiCar", player, paiement)
+    end  
+end
+AddRemoteEvent("taxi:checkbank", CheckBank)]] 
+
+function SpawnTaxiCar(player)
+        -- #1 Check if the player has the taxi job
+        if PlayerData[player].job ~= "taxi" then
+            CallRemoteEvent(player, "MakeErrorNotification", _("not_taxi"))
+            return
+        end
+
+    -- #2 Check if the player has a job vehicle spawned then destroy it
+    if PlayerData[player].job_vehicle ~= nil and ALLOW_RESPAWN_VEHICLE then
+        DestroyVehicle(PlayerData[player].job_vehicle)
+        DestroyVehicleData(PlayerData[player].job_vehicle)
+        PlayerData[player].job_vehicle = nil
+    end
+    
+    -- #3 Try to spawn the vehicle
+    if PlayerData[player].job_vehicle == nil then
+        local spawnPoint = VEHICLE_SPAWN_LOCATION[TaxiGetClosestSpawnPoint(player)]
+        if spawnPoint == nil then return end
+        for k, v in pairs(GetStreamedVehiclesForPlayer(player)) do
+            local x, y, z = GetVehicleLocation(v)
+            if x == false then break end
+            local dist2 = GetDistance3D(spawnPoint.x, spawnPoint.y, spawnPoint.z, x, y, z)
+            if dist2 < 500.0 then
+                CallRemoteEvent(player, "MakeErrorNotification", _("cannot_spawn_vehicle"))
+                return
+            end
+        end
+        local vehicle = CreateVehicle(2, spawnPoint.x, spawnPoint.y, spawnPoint.z, spawnPoint.h)
+        PlayerData[player].job_vehicle = vehicle
+        CreateVehicleData(player, vehicle, 2)
+        SetVehicleRespawnParams(vehicle, false)
+        SetVehiclePropertyValue(vehicle, "locked", true, true)
+        --[[if paiement == "cash" then
+            RemovePlayerCash(player, CAUTION)
+        elseif paiement == "bank" then
+            PlayerData[player].bank_balance = PlayerData[player].bank_balance - CAUTION
+        end]]
+        CallRemoteEvent(player, "MakeNotification", _("spawn_vehicle_success", " taxi car"), "linear-gradient(to right, #00b09b, #96c93d)")
+    else
+        CallRemoteEvent(player, "MakeErrorNotification", _("cannot_spawn_vehicle"))
+    end
+end
+AddRemoteEvent("taxi:spawnvehicle", SpawnTaxiCar)
+
+function DespawnTaxiCar(player)
+    -- Check if the player has a job vehicle spawned then destroy it
+    if PlayerData[player].job_vehicle ~= nil then
+        DestroyVehicle(PlayerData[player].job_vehicle)
+        DestroyVehicleData(PlayerData[player].job_vehicle)
+        PlayerData[player].job_vehicle = nil
+        --PlayerData[player].bank_balance = PlayerData[player].bank_balance + CAUTION
+        CallRemoteEvent(player, "MakeNotification", _("vehicle_stored"), "linear-gradient(to right, #00b09b, #96c93d)")
+        return
     end
 end
 
-function GetCalloutsList(player)
-    if PlayerData[player].job ~= "medic" and PlayerData[player].job ~= "police" then return { } end
-
-    local x,y,z = GetPlayerLocation(player)
-    
-    local calloutsList = {}
-
-    for k,v in pairs(callOuts) do
-        if v.job == PlayerData[player].job then
-            if IsValidPlayer(k) then
-                local dist = math.floor(tonumber(GetDistance2D(x, y, v.location.x, v.location.y)) / 100)
-
-                local taken = v.taken
-
-                if v.taken then
-                    if taken == player then
-                        taken = 'me'
-                    else
-                        taken = PlayerData[taken].name
-                    end
-                end
-
-                table.insert(calloutsList, {
-                    id = k,
-                    from = PlayerData[k].name,
-                    job = v.caller_job,
-                    reason = v.label,
-                    distance = dist,
-                    taken = taken
-                })
+AddEvent("OnPlayerPickupHit", function(player, pickup)-- Store the vehicle in garage
+    if PlayerData[player].job ~= "taxi" then return end
+    for k, v in pairs(TAXI_GARAGE) do
+        if v.garageObject == pickup then
+            local vehicle = GetPlayerVehicle(player)
+            if vehicle == nil then return end
+            local seat = GetPlayerVehicleSeat(player)
+            if vehicle == PlayerData[player].job_vehicle and
+                VehicleData[vehicle].owner == PlayerData[player].accountid and
+                seat == 1
+            then
+                DespawnTaxiCar(player)
             end
         end
     end
+end)
+--------- TAXI VEHICLE END
+--------- INTERACTIONS
 
-    return calloutsList
+function StartCourse(player)
+    local taxiID = GetPlayerVehicle(player)
+    if taxiID ~= 0 then     -- Check if player in vehicle
+        local vehID = GetVehicleModel(taxiID)
+        if vehID == 2 then      -- Check if player in taxi
+            local playerDriver = GetVehicleDriver(taxiID)
+            if player == playerDriver then  -- Check if taximan is driver
+                local occupants = GetClientInTaxi(player, taxiID)     
+                if occupants ~= nil then          -- Check if there is client in taxi
+                    state = true    
+                    for k, v in ipairs(occupants) do
+                        CallRemoteEvent(v, "course", state)
+                    end
+                    CallRemoteEvent(player, "course", state)
+                    TaxiState = true
+                else
+                    CallRemoteEvent(player, "MakeErrorNotification", _("no_player_in_vehicle"))
+                end
+            else
+                CallRemoteEvent(player, "MakeErrorNotification", _("not_driver"))
+            end
+        end
+    else
+        CallRemoteEvent(player, "MakeErrorNotification", _("not_in_vehicle"))
+    end
+end
+AddRemoteEvent("course:start", StartCourse)
+
+--[[function PauseCourse(player)
+    local state = 2
+    for k, v in ipairs(Occupants) do
+        if v ~= 0 then
+            CallRemoteEvent(v, "course", state)
+        end
+    end
+    CallRemoteEvent(player, "course", state)
+end
+AddRemoteEvent("course:pause_unpause", PauseCourse)]]
+
+function StopCourse(player)
+    if TaxiState then
+        state = false
+        CallRemoteEvent(player, "course", state)
+        CallRemoteEvent(player, "MakeNotification", _("process_pay"), "linear-gradient(to right, #00b09b, #96c93d)")
+        for k, v in ipairs(occupants) do
+            CallRemoteEvent(v, "course", state)
+            CallRemoteEvent(v, "MakeNotification", _("process_pay"), "linear-gradient(to right, #00b09b, #96c93d)")
+        end
+    else
+        CallRemoteEvent(player, "MakeErrorNotification", _("no_current_course"))
+    end
+end
+AddRemoteEvent("course:stop", StopCourse)
+
+AddRemoteEvent("notifCash", function(player)
+    if TaxiState then
+        TaxiState = false
+        CallRemoteEvent(player, "MakeNotification", _("cash_taxi"), "linear-gradient(to right, #00b09b, #96c93d)")          -- Les joueurs doivent procéder au paiement depuis leur inventaires
+        CallRemoteEvent(player, "HideTaxiHud")
+        for k, v in ipairs(occupants) do
+            CallRemoteEvent(v, "MakeNotification", _("cash_taxi"), "linear-gradient(to right, #00b09b, #96c93d)")
+            CallRemoteEvent(v, "HideTaxiHud")
+        end
+        occupants = nil     -- clear table
+    end
+end)
+
+AddRemoteEvent("bankPay", function(player, CourseTime)
+    if TaxiState then
+        TaxiState = false
+        for k, v in ipairs(occupants) do
+            PlayerData[v].bank_balance = PlayerData[v].bank_balance - CourseTime                -- On débite les clients
+            PlayerData[player].bank_balance = PlayerData[player].bank_balance + CourseTime      -- On alimente le compte du chauffeur
+            CallRemoteEvent(v, "MakeNotification", _("pay_success"), "linear-gradient(to right, #00b09b, #96c93d)")
+        end
+        CallRemoteEvent(player, "MakeNotification", _("pay_success"), "linear-gradient(to right, #00b09b, #96c93d)")
+        occupants = nil     -- clear table
+    end
+end)
+
+--------- INTERACTIONS END
+-- Tools
+function TaxiGetClosestSpawnPoint(player)
+    local x, y, z = GetPlayerLocation(player)
+    local closestSpawnPoint
+    local dist
+    for k, v in pairs(VEHICLE_SPAWN_LOCATION) do
+        local currentDist = GetDistance3D(x, y, z, v.x, v.y, v.z)
+        if (dist == nil or currentDist < dist) and currentDist <= 2000 then
+            closestSpawnPoint = k
+            dist = currentDist
+        end
+    end
+    return closestSpawnPoint
 end
 
---------- CALLOUTS END
+function GetClientInTaxi(player, taxiID)
+    for i = 2, 4 do
+        local passenger = GetVehiclePassenger(taxiID, i)
+        if (passenger ~= 0) then
+            table.insert( occupants, passenger )
+            --occupants [#occupants + 1] = passenger
+		end
+    end
+    if #occupants == 0 then
+        return nil
+    else
+        return occupants
+    end
+end
